6a2cecd00e3d351f52d531af652ea06e16badf10
David Frank <bitinn@users.noreply.github.com>
2023-03-12 01:04:58 +0800

    debug

--- index.js
+++ index.js
@@ -15,24 +15,25 @@ bot.on("inline_query", async({ inlineQuery, answerInlineQuery }) => {
     const link = inlineQuery.query;
 
     if (!link) {
-        return answerInlineQuery([]);
+        return answerInlineQuery([], {});
     }
 
     const url = new URL(link);
 
     if (!settings.domains.includes(url.hostname)) {
-        return answerInlineQuery([]);
+        return answerInlineQuery([], {});
     }
 
     const browser = await puppeteer.launch();
+    console.log(link);
 
     // optimize this?
     try {
         const page = await browser.newPage();
         await page.goto(url);
 
-        const images = await page.waitForSelector(settings.thumbnail, { timeout: 5000 });
-        const texts = await page.waitForSelector(settings.text, { timeout: 5000 });
+        const images = await page.waitForSelector(settings.thumbnail, { timeout: settings.timeout });
+        const texts = await page.waitForSelector(settings.text, { timeout: settings.timeout });
 
         const image = await images.evaluate(el => el.href);
         const text = await texts.evaluate(el => el.textContent);
@@ -47,11 +48,11 @@ bot.on("inline_query", async({ inlineQuery, answerInlineQuery }) => {
 
         console.log(results);
 
-        return answerInlineQuery(results);
+        return answerInlineQuery(results, {});
     } catch (err) {
         browser.close();
         console.error(err);
-        return answerInlineQuery([]);
+        return answerInlineQuery([], {});
     }
 });
 
