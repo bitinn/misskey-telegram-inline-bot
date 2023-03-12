ab837227eeb633907856beb5bddc4cf4c1a26500
David Frank <bitinn@users.noreply.github.com>
2023-03-12 15:30:05 +0800

    better async

--- misskey.js
+++ misskey.js
@@ -10,9 +10,69 @@ const settings = Object.assign({}, defaults, configs);
 const sha256 = x => crypto.createHash('sha256').update(x, 'utf8').digest('hex');
 
 const bot = new Telegraf(settings.token);
+let browser;
+
+async function getData(browser, link, start) {
+    const page = await browser.newPage();
+    console.log("open tab: " + link);
+
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+        if (req.resourceType() === 'image') {
+            req.abort();
+        } else {
+            req.continue();
+        }
+    });
+
+    var to = setTimeout(async() => {
+        if (page && !page.isClosed()) {
+            await page.close();
+        }
+    }, settings.tab);
+
+    await page.goto(link, { waitUntil: "domcontentloaded", timeout: settings.timeout });
+    console.log("open page: " + link);
+
+    const title = await page.waitForSelector(settings.title, { timeout: settings.timeout });
+    const files = await page.waitForSelector(settings.click, { timeout: settings.timeout });
+    const name = await title.evaluate(el => el.textContent);
+    await files.click();
+    console.log("click files: " + link);
+
+    const texts = await page.waitForSelector(settings.text, { timeout: settings.timeout });
+    const thumbs = await page.waitForSelector(settings.thumbnail, { timeout: settings.timeout });
+    const images = await page.waitForSelector(settings.image, { timeout: settings.timeout });
+    const text = await texts.evaluate(el => el.textContent);
+    const thumb = await thumbs.evaluate(el => el.src);
+    const image = await images.evaluate(el => el.href);
+    console.log("got data: " + link);
+
+    const end = Date.now();
+    console.log("time taken: " + (end - start) + " " + link);
+
+    await page.close();
+    clearTimeout(to);
+
+    return {
+        type: 'article',
+        id: sha256(link),
+        title: name,
+        description: text,
+        url: link,
+        thumbnail_url: thumb,
+        input_message_content: {
+            message_text: name + " / " + text + " / [image](" + image + ") / [post](" + link + ")",
+        },
+    };
+}
 
 bot.on("inline_query", async(ctx) => {
     const link = ctx.inlineQuery.query;
+
+    if (!browser) {
+        return ctx.answerInlineQuery([], { cache_time: settings.cache_time });
+    }
 
     if (!link) {
         return ctx.answerInlineQuery([], { cache_time: settings.cache_time });
@@ -29,64 +89,41 @@ bot.on("inline_query", async(ctx) => {
         return ctx.answerInlineQuery([], { cache_time: settings.cache_time });
     }
 
-    const browser = await puppeteer.launch();
-    console.log(link);
-
     // optimize this?
     try {
-        const start = Date.now();
-        console.log(start);
+        const data = await getData(browser, link, start);
+        console.log(data);
 
-        const page = await browser.newPage();
-        await page.goto(link, { waitUntil: 'domcontentloaded' });
-
-        const title = await page.waitForSelector("title", { timeout: settings.timeout });
-        const name = await title.evaluate(el => el.textContent);
-        console.log(name);
-
-        const files = await page.waitForSelector(settings.click, { timeout: settings.timeout });
-        console.log("found " + settings.click);
-        await files.click();
-
-        const images = await page.waitForSelector(settings.thumbnail, { timeout: settings.timeout });
-        const texts = await page.waitForSelector(settings.text, { timeout: settings.timeout });
-        console.log("found " + settings.thumbnail);
-
-        const image = await images.evaluate(el => el.href);
-        const text = await texts.evaluate(el => el.textContent);
-
-        console.log(image);
-        console.log(text);
-
-        const results = [{
-            type: 'article',
-            id: sha256(link),
-            title: name,
-            description: text,
-            url: link,
-            thumbnail_url: image,
-            input_message_content: {
-                message_text: name + " / " + text + " / " + image + " / " + link
-            }
-        }];
-
-        console.log(results);
-        browser.close();
-
-        const end = Date.now();
-        console.log(end - start);
-
-        return ctx.answerInlineQuery(results, { cache_time: settings.cache_time });
+        return ctx.answerInlineQuery([data], { cache_time: settings.cache_time });
     } catch (err) {
         console.error(err);
-        browser.close();
 
         return ctx.answerInlineQuery([], { cache_time: settings.cache_time });
     }
 });
 
-bot.startPolling();
-
 bot.catch((err) => {
-    console.error(err)
-})
+    console.error(err);
+});
+
+(async() => {
+    bot.launch();
+
+    const start = Date.now();
+    console.log(start);
+
+    browser = await puppeteer.launch();
+
+    const end = Date.now();
+    console.log("app start: " + (end - start));
+})();
+
+process.once('SIGINT', () => {
+    bot.stop('SIGINT');
+    browser.close();
+});
+
+process.once('SIGTERM', () => {
+    bot.stop('SIGTERM');
+    browser.close();
+});
--- test.js
+++ test.js
@@ -1,44 +1,89 @@
 
 const puppeteer = require("puppeteer");
+const crypto = require("crypto");
 
 const defaults = require(__dirname + "/settings-default.json");
 const configs = require(__dirname + "/settings.json");
 
 const settings = Object.assign({}, defaults, configs);
 
+const links = [
+    "https://misskey.io/notes/9c62xgczwe",
+    "https://misskey.io/notes/9c7kxepaks",
+    "https://misskey.io/notes/9c8bc5l311"
+];
+
+const sha256 = x => crypto.createHash('sha256').update(x, 'utf8').digest('hex');
+
+async function getData(browser, link, start) {
+    const page = await browser.newPage();
+    console.log("open tab: " + link);
+
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+        if (req.resourceType() === 'image') {
+            req.abort();
+        } else {
+            req.continue();
+        }
+    });
+
+    var to = setTimeout(async() => {
+        if (page && !page.isClosed()) {
+            await page.close();
+        }
+    }, settings.tab);
+
+    await page.goto(link, { waitUntil: "domcontentloaded", timeout: settings.timeout });
+    console.log("open page: " + link);
+
+    const title = await page.waitForSelector(settings.title, { timeout: settings.timeout });
+    const files = await page.waitForSelector(settings.click, { timeout: settings.timeout });
+    const name = await title.evaluate(el => el.textContent);
+    await files.click();
+    console.log("click files: " + link);
+
+    const texts = await page.waitForSelector(settings.text, { timeout: settings.timeout });
+    const thumbs = await page.waitForSelector(settings.thumbnail, { timeout: settings.timeout });
+    const images = await page.waitForSelector(settings.image, { timeout: settings.timeout });
+    const text = await texts.evaluate(el => el.textContent);
+    const thumb = await thumbs.evaluate(el => el.src);
+    const image = await images.evaluate(el => el.href);
+    console.log("got data: " + link);
+
+    const end = Date.now();
+    console.log("time taken: " + (end - start) + " " + link);
+
+    await page.close();
+    clearTimeout(to);
+
+    return {
+        type: 'article',
+        id: sha256(link),
+        title: name,
+        description: text,
+        url: link,
+        thumbnail_url: thumb,
+        input_message_content: {
+            message_text: name + " / " + text + " / [image](" + image + ") / [post](" + link + ")",
+        },
+    };
+}
+
 (async() => {
-    const link = "https://misskey.io/notes/9c62xgczwe";
+    const start = Date.now();
+    console.log(start);
+
     const browser = await puppeteer.launch();
-    console.log(link);
 
-    // optimize this?
+    const end = Date.now();
+    console.log("session start: " + (end - start));
+
     try {
-        const start = Date.now();
-        console.log(start);
-
-        const page = await browser.newPage();
-        await page.goto(link);
-
-        const title = await page.waitForSelector("title", { timeout: settings.timeout });
-        const name = await title.evaluate(el => el.textContent);
-        console.log(name);
-
-        const files = await page.waitForSelector(settings.click, { timeout: settings.timeout });
-        console.log("found " + settings.click);
-        await files.click();
-
-        const images = await page.waitForSelector(settings.thumbnail, { timeout: settings.timeout });
-        const texts = await page.waitForSelector(settings.text, { timeout: settings.timeout });
-        console.log("found " + settings.thumbnail);
-
-        const image = await images.evaluate(el => el.href);
-        const text = await texts.evaluate(el => el.textContent);
-
-        console.log(image);
-        console.log(text);
-
-        const end = Date.now();
-        console.log(end - start);
+        await Promise.all(links.map(async (link) => {
+            const data = await getData(browser, link, start);
+            console.log(data);
+        }));
     } catch (err) {
         console.error(err);
     }
