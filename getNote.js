34b93ecd3493140a32182a1695647c81186a9a64
David Frank <bitinn@users.noreply.github.com>
2023-04-05 01:47:12 +0800

    use api instead

---
+++ getNote.js
@@ -0,0 +1,60 @@
+
+const { md } = require("telegram-escape");
+const crypto = require("crypto");
+const fetch = require("node-fetch");
+
+const defaults = require(__dirname + "/settings-default.json");
+const configs = require(__dirname + "/settings.json");
+const settings = Object.assign({}, defaults, configs);
+
+const sha256 = x => crypto.createHash('sha256').update(x, 'utf8').digest('hex');
+
+module.exports = async (query) => {
+    let url = new URL(query);
+    if (!settings.domains.includes(url.hostname)) {
+        return;
+    }
+    const link = url.href;
+    console.log("valid url: " + link);
+
+    const id = url.pathname.split("/").at(-1);
+    console.log("note id: " + id);
+
+    url.pathname = "/api/notes/show";
+    const data = await fetch(url.href, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ "noteId": id }),
+        timeout: settings.timeout,
+    }).then(res => res.json());
+    //console.log(data);
+
+    let name = data.user.name;
+    let text = data.text;
+
+    let thumb = "";
+    let image = "";
+
+    if (data.files && data.files.length > 0) {
+        image = data.files[0].url;
+        thumb = data.files[0].thumbnailUrl;
+    }
+
+    return {
+        type: 'article',
+        id: sha256(link),
+        title: name,
+        description: text,
+        url: link,
+        thumbnail_url: thumb,
+        input_message_content: {
+            message_text:
+                image ?
+                    md`${name} / ${text} / [image](${image}) / [post](${link})` :
+                    url ?
+                        md`${name} / ${text} / [link](${url})` :
+                        md`${name} / ${text}`,
+            parse_mode: "MarkdownV2",
+        },
+    };
+};
--- misskey.js
+++ misskey.js
@@ -1,127 +1,24 @@
 
-const { Telegraf } = require("telegraf");
-const puppeteer = require("puppeteer");
-const { md } = require("telegram-escape");
-const crypto = require("crypto");
+const getNote = require("./getNote");
 
 const defaults = require(__dirname + "/settings-default.json");
 const configs = require(__dirname + "/settings.json");
-
 const settings = Object.assign({}, defaults, configs);
-const sha256 = x => crypto.createHash('sha256').update(x, 'utf8').digest('hex');
 
 const bot = new Telegraf(settings.token);
-let browser;
-
-async function getData(browser, link, start) {
-    const page = await browser.newPage();
-    console.log("open tab: " + link);
-
-    await page.setRequestInterception(true);
-    page.on('request', (req) => {
-        if (req.resourceType() === 'image') {
-            req.abort();
-        } else {
-            req.continue();
-        }
-    });
-
-    var to = setTimeout(async() => {
-        if (page && !page.isClosed()) {
-            await page.close();
-        }
-    }, settings.tab);
-
-    await page.goto(link, { waitUntil: "load", timeout: settings.timeout });
-    console.log("open page: " + link);
-
-    const title = await page.waitForSelector(settings.title, { timeout: settings.timeout });
-    const name = await title.evaluate(el => el.textContent);
-    console.log("get title: " + name);
-
-    let text = "";
-    let url = "";
-    try {
-        const texts = await page.waitForSelector(settings.text, { timeout: settings.timeout });
-        text = await texts.evaluate(el => el.textContent);
-
-        const links = await page.waitForSelector(settings.link, { timeout: settings.find });
-        url = await links.evaluate(el => el.href);
-    } catch (err) {
-        // skip
-    }
-
-    let thumb = "";
-    let image = "";
-    try {
-        const files = await page.waitForSelector(settings.click, { timeout: settings.timeout });
-        await files.click();
-        console.log("click files: " + link);
-
-        const images = await page.waitForSelector(settings.image, { timeout: settings.find });
-        image = await images.evaluate(el => el.href);
-
-        const thumbs = await page.waitForSelector(settings.thumbnail, { timeout: settings.find });
-        thumb = await thumbs.evaluate(el => el.src);
-    } catch (err) {
-        // skip
-    }
-
-    console.log("got data: " + (image ? "image" : "link"));
-
-    const end = Date.now();
-    console.log("time taken: " + (end - start) + " " + link);
-
-    await page.close();
-    clearTimeout(to);
-
-    return {
-        type: 'article',
-        id: sha256(link),
-        title: name,
-        description: text,
-        url: link,
-        thumbnail_url: thumb,
-        input_message_content: {
-            message_text:
-                image ?
-                    md`${name} / ${text} / [image](${image}) / [post](${link})` :
-                    url ?
-                        md`${name} / ${text} / [link](${url})` :
-                        md`${name} / ${text}`,
-            parse_mode: "MarkdownV2",
-        },
-    };
-}
 
 bot.on("inline_query", async(ctx) => {
     const start = Date.now();
     console.log(start);
 
     const link = ctx.inlineQuery.query;
-
-    if (!browser) {
-        return ctx.answerInlineQuery([], { cache_time: settings.cache_time });
-    }
-
     if (!link) {
-        return ctx.answerInlineQuery([], { cache_time: settings.cache_time });
-    }
-
-    let url;
-    try {
-        url = new URL(link);
-    } catch (err) {
-        return ctx.answerInlineQuery([], { cache_time: settings.cache_time });
-    }
-
-    if (!settings.domains.includes(url.hostname)) {
         return ctx.answerInlineQuery([], { cache_time: settings.cache_time });
     }
 
     // optimize this?
     try {
-        const data = await getData(browser, link, start);
+        const data = await getNote(link);
         console.log(data);
 
         return ctx.answerInlineQuery([data], { cache_time: settings.cache_time });
@@ -142,18 +39,14 @@ bot.catch((err) => {
     const start = Date.now();
     console.log(start);
 
-    browser = await puppeteer.launch();
-
     const end = Date.now();
     console.log("app start: " + (end - start));
 })();
 
 process.once('SIGINT', () => {
     bot.stop('SIGINT');
-    browser.close();
 });
 
 process.once('SIGTERM', () => {
     bot.stop('SIGTERM');
-    browser.close();
 });
